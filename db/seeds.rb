a40c44f21b0a73495d4beb2d42e22a3e4e214683
AdrienMeyer <reyemneirda@gmail.com>
2020-09-23 12:52:49 +0300

    Set seed to import json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require "json"
+# lines = File.open("db/recipes.json").read.split("}\n").join("},")
+
+# data = ActiveSupport::JSON.decode([lines].to_s) 
+
+File.open('db/recipes.json', 'r') do |file|
+    file.each do |line|
+        user_attrs = JSON.parse line
+        Recipe.create!(
+            rate: user_attrs['rate'],
+            author_tip: user_attrs['author_tip'],
+            budget: user_attrs['budget'],
+            prep_time: user_attrs['prep_time'],
+            name: user_attrs['name'],
+            author: user_attrs['author'],
+            difficulty: user_attrs['difficulty'],
+            people_quantity: user_attrs['people_quantity'],
+            cook_time: user_attrs['cook_time'],
+            total_time: user_attrs['total_time'],
+            image: user_attrs['image'],
+            nb_comment: user_attrs['nb_comment'],
+            tags: user_attrs['tags'],
+            ingredients: user_attrs['ingredients'],
+        )
+    end
+end
