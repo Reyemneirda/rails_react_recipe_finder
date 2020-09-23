3c31c8fb018f170f66009f5ef13a166b68c03719
AdrienMeyer <reyemneirda@gmail.com>
2020-09-23 12:53:02 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v0 do
+      get 'recipes/index'
+      get 'recipes/search'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
   root 'pages#index'
+  get '/*path' => 'pages#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
