b546140e9b648d1ec93dff890f054c130e6b8e64
AdrienMeyer <reyemneirda@gmail.com>
2020-09-23 12:54:35 +0300

    Add index to router

--- app/javascript/router/Index.jsx
+++ app/javascript/router/Index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "../components/HomePage";
+import Recipes from "../components/Recipes/Index";
+
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={HomePage} />
+      <Route path="/recipes" exact component={Recipes} />
     </Switch>
   </Router>
 );
