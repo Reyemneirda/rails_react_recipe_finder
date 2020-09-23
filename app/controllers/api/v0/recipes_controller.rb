15d06084e1e0d2f81eb5d3fd80e4f5fdf82a241a
AdrienMeyer <reyemneirda@gmail.com>
2020-09-23 12:53:24 +0300

    Add functions to controller

---
+++ app/controllers/api/v0/recipes_controller.rb
@@ -0,0 +1,29 @@
+class Api::V0::RecipesController < ApplicationController
+    def index
+      recipe = Recipe.all.order(created_at: :desc)
+      render json: recipe
+    end
+
+    def search
+      if params['q'].present?
+        @query = params['q']
+        recipe = Recipe.where("lower(name) like ?", "%#{@query.downcase}%").or(Recipe.where("array_to_string(ingredients, '||') LIKE ?", "%#{@query.downcase}%" "%gem%")).or(Recipe.where("array_to_string(tags, '||') LIKE ?", "%#{@query.downcase}%" "%gem%"))
+        render json: recipe
+      end
+    end
+
+    def create
+    end
+  
+    def show
+    end
+  
+    def destroy
+    end
+    private
+
+  def recipe_params
+    params.permit(:name, :image, :ingredients, :tags, :rate, :author_tip, :budget, :prep_time, :name, 
+    :author, :difficulty, :people_quantity, :cook_time, :total_time, :image, :nb_comment)
+  end
+  end
